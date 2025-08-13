5ac657c76943fabe49c93ee839b6c9cd45dd5b17
jdelarubia <jdelarubia@gmail.com>
2025-08-13 21:37:34 +0200

    refactor function

--- helpers.py
+++ helpers.py
@@ -8,13 +8,21 @@ DATA_DIR.mkdir(exist_ok=True)
 
 
 def normalize_column_name(col_name: str) -> str:
-    """MISC. Normalize column names by:
-    - converting to lowercase
+    """MISC.
+    Given a string (column names), normalize it by:
+    - converting it to lowercase
     - adding an underscore between words
-    - removing () symbols"""
-    return "_".join(
-        col_name.lower().replace(".", "").replace("(", "").replace(")", "").split()
-    )
+    - removing symbols such as: ['(', ')', '.']"""
+
+    def remove_symbols(col_name: str) -> str:
+        symbols = ["(", ")", "."]
+        col_name = col_name.strip().lower()
+
+        for symbol in symbols:
+            col_name = col_name.replace(symbol, "")
+        return "_".join(col_name.split())
+
+    return remove_symbols(col_name)
 
 
 if __name__ == "__main__":
